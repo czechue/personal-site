2fdfe3005200217f4eb13e3d7a7c5883c36c28bf
Michal Lester <michal.lester@allegro.pl>
2020-03-21 07:39:56 +0100

    initial commit

--- src/site/index.md
+++ src/site/index.md
@@ -1,6 +1,7 @@
 ---
 layout: home
 title: Welcome
+funy: hey
 ---
 
 Welcome to Eleventy starter.
--- src/utils/htmlmin.js
+++ src/utils/htmlmin.js
@@ -2,12 +2,11 @@ const htmlmin = require('html-minifier')
 
 module.exports = function(content, outputPath) {
   if (outputPath.endsWith('.html')) {
-    let minified = htmlmin.minify(content, {
+    return htmlmin.minify(content, {
       useShortDoctype: true,
       removeComments: true,
       collapseWhitespace: true,
     })
-    return minified
   }
 
   return content
